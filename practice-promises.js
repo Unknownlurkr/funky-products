23169264dcd660cbb6a63167ec40e2d7ab9acb9d
Unknownlurkr <b.samantha.samuels@gmail.com>
2020-03-27 11:14:17 -0400

    changing img source

--- practice-promises.js
+++
@@ -1,9 +0,0 @@
-fetch('strange-products.json').then(function(response){
-    return response.json();
-    }).the(function(json){
-        products = json;
-        intitilize();
-    }).catch(function(err){
-        console.log('Fetch problem:' + err.message);
-    });
-    
