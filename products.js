2a66f32ed40851b8a1d525494bb61ca2980fbc04
Unknownlurkr <b.samantha.samuels@gmail.com>
2020-03-27 10:54:49 -0400

    fixed up code more

--- products.js
+++ products.js
@@ -23,7 +23,7 @@ function strangeProducts(jsonObj){
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
         let section = document.querySelector('section');
-        img.setAttribute('src', 'https://unknownlurkr.github.io/funky-products/img/' + strangeProd[i].img);
+        img.setAttribute('src', 'https://github.com/Unknownlurkr/funky-products/tree/master/img/' + strangeProd[i].img);
         img.setAttribute('alt', strangeProd[i].name);
         h2.textContent = strangeProd[i].name;
         p1.textContent = 'details' + strangeProd[i].details;
@@ -32,7 +32,7 @@ function strangeProducts(jsonObj){
         info.appendChild(h2);
         info.appendChild(p1);
         info.appendChild(p2);
-        section.appendChild(article);
+        section.appendChild(info);
 
     }
 }
