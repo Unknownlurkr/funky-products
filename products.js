1137142725c3599c82cc5938ab9739c5ed826667
Unknownlurkr <b.samantha.samuels@gmail.com>
2020-03-27 11:06:53 -0400

    changing img source

--- products.js
+++ products.js
@@ -23,7 +23,7 @@ function strangeProducts(jsonObj){
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
         let section = document.querySelector('section');
-        img.setAttribute('src', 'C:/Users/bsamu/Documents/funky-products/img' + strangeProd[i].img);
+        img.setAttribute('src', 'https://unknownlurkr.github.io/funky-products/img/' + strangeProd[i].img);
         img.setAttribute('alt', strangeProd[i].name);
         h2.textContent = strangeProd[i].name;
         p1.textContent = 'details' + strangeProd[i].details;
